528cec59c0109ec7681f86376e76b91d1aeabb51
Demian Flavius <flaviusdemian91@yahoo.com>
2014-08-28 12:24:15 +0300

    refactoring

---
+++ Xamarin.MobileServices/MyApplication/MyApplication.cs
@@ -0,0 +1,45 @@
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.MobileServices.Core;
+using Message = Android.OS.Message;
+
+namespace Xamarin.MobileServices.Android.MyApplication
+{
+    [Application(Debuggable = true)]
+    public class MyApplication : Application
+    {
+        public static MobileServiceClient MobileService =
+            new MobileServiceClient("https://demoflavius.azure-mobile.net/", "BAnpBgjwvAiseBuqYzJohLfKjwSWbD28");
+
+        public static String Channel = null;
+
+        public static readonly IMobileServiceTable<UserEntity> UsersTable = MobileService.GetTable<UserEntity>();
+        public static readonly IMobileServiceTable<Message> MessagesTable = MobileService.GetTable<Message>();
+        public static readonly IMobileServiceTable<Channel> ChannelsTable = MobileService.GetTable<Channel>();
+
+        public static MobileServiceUser MobileServicesUser = null;
+        public static UserEntity CurrentUser = new UserEntity();
+        public static Channel CurrentChannel = new Channel();
+
+        public MyApplication(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+    
+        public override void OnCreate()
+        {
+            try
+            {
+                base.OnCreate();
+                // do application specific things here
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+    }
+}
